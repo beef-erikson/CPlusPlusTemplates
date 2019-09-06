9cbb783392fdd429a1698b72b5f1fdb8de8b1b07
Troy Martin <beef.erikson.studios@gmail.com>
2019-09-05 20:20:52 -0400

    Assignment1 finished

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -1,20 +1,54 @@
-// Assignment1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Assignment1.cpp
+//
+// Convert the following functions into templates
+// 1. int Add(int x, int y)
+// 2. int ArraySum(int* pArr, int arrSize)
+// 3. int Max(int* pArr, int arrSize)
 //
 
 #include <iostream>
 
-int main()
+
+template<typename T>
+T Add(T x, T y)
 {
-    std::cout << "Hello World!\n";
+	return x + y;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+template<typename T>
+T ArraySum(T* pArr, T arrSize)
+{
+	auto sum = 0;
+	for (int i = 0; i < arrSize; i++)
+	{
+		sum += pArr[i];
+	}
+	return sum;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+template<typename T>
+T ArrayMax(T* pArr, T arrSize)
+{
+	auto max = 0;
+	for (int i = 0; i < arrSize; i++)
+	{
+		if (pArr[i] > max)
+		{
+			max = pArr[i];
+		}
+	}
+	return max;
+}
+
+int main()
+{
+	auto numAdd = Add(12, 24);
+	std::cout << numAdd << std::endl;
+
+	int numArray[] = { 12, 234, 235, 22 };
+	auto arraySum = ArraySum(numArray, 4);
+	std::cout << arraySum << std::endl;
+
+	auto arrayMax = ArrayMax(numArray, 4);
+	std::cout << arrayMax << std::endl;
+}
--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 [Udemy C++ Templates Course](https://www.udemy.com/course/beg-cpp-temp/)
 
 ## Function Templates
-[Introduction to Templates](https://github.com/beef-erikson/CPlusPlusTemplates/tree/master/IntroToTemplates)
+[Introduction to Templates](https://github.com/beef-erikson/CPlusPlusTemplates/blob/master/IntroToTemplates/IntroToTemplates.cpp)<br />
+[Assignment 1](https://github.com/beef-erikson/CPlusPlusTemplates/blob/master/Assignment1/Assignment1.cpp)<br />
+
